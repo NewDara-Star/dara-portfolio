deb9a3622e7879bff45ef0a621f86e16d19f64ad
NewDara-Star <139634597+NewDara-Star@users.noreply.github.com>
2025-01-05 23:10:12 +0000

    relocating image files

--- _codux/boards/stack-component/stack-component.board.tsx
+++ _codux/boards/stack-component/stack-component.board.tsx
@@ -1,11 +1,12 @@
 import { createBoard } from '@wixc3/react-board';
 import { StackComponent } from '../../../src/components/stack-component/stack-component';
+import styles from './stack-component.board.module.scss';
 
 export default createBoard({
     name: 'StackComponent',
-    Board: () => <StackComponent />,
+    Board: () => <StackComponent className={styles.stackComponent} />,
     environmentProps: {
         windowWidth: 509,
-        windowHeight: 230,
+        windowHeight: 387,
     },
 });
--- src/components/stack-component/stack-component.tsx
+++ src/components/stack-component/stack-component.tsx
@@ -14,19 +14,25 @@ export const StackComponent = ({ className }: StackComponentProps) => {
         <div className={cx(styles.root, className)}>
             <div className={styles.cardStack}>
                 <div className={styles.smallTag}>
-                    <img src="public/techStack.png" alt="stack icon" />
+                    <img src="src/assets/img/noImage/techStack.svg" alt="stack icon" />
                     Tech stack
                 </div>
+                <div className={styles.IconSpan}>
+                    <img src="src/assets/img/noImage/skill-icons_photoshop.png" alt="" />
+                    <img src="src/assets/img/noImage/skill-icons_javascript.png" alt="" />
+                    <img src="src/assets/img/noImage/skill-icons_illustrator.png" alt="" />
+                    <img src="src/assets/img/noImage/logos_python.png" alt="" />
+                    <img src="src/assets/img/noImage/logos_html-5.png" alt="" />
+                    <img src="src/assets/img/noImage/logos_figma.png" alt="" />
+                    <img src="src/assets/img/noImage/logos_css-3.png" alt="" />
+                </div>
+                <h3>Techstack</h3>
+                <p className={styles.p1}>
+                    Lorem ipsum dolor sit amet consectetur. Etiam sed non tortor euismod viverra
+                    nulla. Condimentum interdum elementum odio id integer amet suspendisse.
+                    Pellentesque.
+                </p>
             </div>
-            <span className={styles.IconSpan}>
-                <img src="public/skill-icons_photoshop.png" alt="" />
-                <img src="public/skill-icons_javascript.png" alt="" />
-                <img src="public/skill-icons_illustrator.png" alt="" />
-                <img src="public/logos_python.png" alt="" />
-                <img src="public/logos_html-5.png" alt="" />
-                <img src="public/logos_figma.png" alt="" />
-                <img src="public/logos_css-3.png" alt="" />
-            </span>
         </div>
     );
 };
